e9321c400dfbd9d13718d5bc32123379c22b7b78
ltann <leeroyleeroy18@gmail.com>
2019-03-06 20:20:09 -0800

    Added dotenv file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // code away!
+require('dotenv').config()
+
 const server = require('./server')
-const port = 9002
+
+const port = process.env.PORT || 9002
 
 server.listen(port, () => console.log(`Listening to port ${port}`))
