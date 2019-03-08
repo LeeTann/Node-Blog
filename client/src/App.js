671f5a3a8b73c0367620e689c56196d756e02740
ltann <leeroyleeroy18@gmail.com>
2019-03-07 19:54:53 -0800

    Connect react to heroku server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  getUsers = () => {
+    axios.get('https://lee-tann-node-blog.herokuapp.com/api/users')
+    .then(res => {
+      console.log(res)
+      this.setState(() => ({ users: res.data }))
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  componentDidMount() {
+    this.getUsers()
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <p>Node Blog</p>
+        <p>Lee's Node Blog</p>
+        <p>USERS:</p>
+        {this.state.users.map(user => {
+          return(
+            <p key={user.id}>NAME: {user.name}</p>
+          )
+        })}
       </div>
     );
   }
